5bbc858895196dcd94625b82869e5d00ee7027e5
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2022-10-19 20:09:43 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    
+    for(int i = 0; i < N ; i++) {
+        int a, b;
+        cin >> a >> b;
+    }
+    
+    cout << "비와이\n";
+}
