eb377326b8c507886f2e071d95a2318561a65136
HOYA <66549638+chominho96@users.noreply.github.com>
2023-09-16 20:36:17 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    FASTIO;
+
+    cout << "         ,r'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+}
