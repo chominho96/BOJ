0ac620711ad11bc47c8eed30072b54b33e57d003
HOYA <66549638+chominho96@users.noreply.github.com>
2023-10-24 13:07:18 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string input; cin >> input;
+    if (input == "SONGDO") cout << "HIGHSCHOOL\n";
+    if (input == "CODE") cout << "MASTER\n";
+    if (input == "2023") cout << "0611\n";
+    if (input == "ALGORITHM") cout << "CONTEST\n";
+}
