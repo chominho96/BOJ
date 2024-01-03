74a19755e395880a3047fcc75d0a9aea4f9b651a
HOYA <66549638+chominho96@users.noreply.github.com>
2024-01-03 12:50:37 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.py
@@ -0,0 +1,15 @@
+def func():
+    answer = 0
+    
+    while True:
+        try:
+            gum_gum = input()
+            answer += 1
+        except EOFError:
+            break
+    
+    return answer
+
+
+if __name__ == "__main__":
+    print(func())
