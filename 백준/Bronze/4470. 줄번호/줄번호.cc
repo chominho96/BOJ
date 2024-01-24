9ff66cf22197764e3d7e91fbf995d424c342a703
HOYA <66549638+chominho96@users.noreply.github.com>
2024-01-24 11:57:16 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    cin.ignore();
+    for (int i = 1; i <= N; i++) {
+        string str; getline(cin, str);
+        cout << i << ". " << str << '\n';
+    }
+}
