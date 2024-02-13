4145fd309fdc55330eecb4355b9e36dc525d1ecc
HOYA <66549638+chominho96@users.noreply.github.com>
2024-02-13 15:26:34 +0900

    [Silver IV] Title: 수들의 합 2, Time: 40 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -1,30 +1,47 @@
 #include <iostream>
 #define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define MAX_SIZE 10001
+#define MAX_SIZE 10010
 
 using namespace std;
 
 int N, M;
 int NUMBER[MAX_SIZE];
+int SUM = 0;
 int ans = 0;
 
-void calc(int idx, int value) {
-    if (value == M) {
-        ans++;
-        return;
+void calc() {
+    int left = 1;
+    int right = N;
+    while (true) {
+        if (left > right) break;
+        if (SUM == M) {
+            ans++;
+            break;
+        }
+        int sum = SUM;
+        int l = left;
+        int r = right;
+        while (sum > M && l <= r) {
+            sum -= NUMBER[r--];
+        }
+        if (sum == M) ans++;
+        r = right;
+        sum = SUM;
+        while (sum > M && l <= r) {
+            sum -= NUMBER[l++];
+        }
+        if (sum == M) ans++;
+        SUM -= (NUMBER[left++] + NUMBER[right--]);
     }
-    if (value > M || idx > N) return;
-    calc(idx + 1, value + NUMBER[idx + 1]);
 }
 
 int main() {
-    FASTIO
+    FASTIO;
     cin >> N >> M;
     for (int i = 1; i <= N; i++) {
         cin >> NUMBER[i];
+        SUM += NUMBER[i];
     }
-    for (int i = 1; i <= N; i++) {
-        calc(i, NUMBER[i]);
-    }
+    calc();
     cout << ans << '\n';
 }
