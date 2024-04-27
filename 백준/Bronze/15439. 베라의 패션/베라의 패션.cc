66518f6763cbb44b39e54234846e1657222f94f8
HOYA <66549638+chominho96@users.noreply.github.com>
2024-04-27 10:21:05 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    int N; cin >> N; cout << N * (N - 1) << endl;
+}
