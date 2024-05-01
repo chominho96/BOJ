7161f3dddc3bbbb56d492bff5f7a0ccc4439193d
HOYA <66549638+chominho96@users.noreply.github.com>
2024-05-01 19:25:52 +0900

    [Bronze IV] Title: Tabliczka, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8674. Tabliczka/Tabliczka.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+long long a[2];
+
+int main(){
+    cin >> a[0] >> a[1];
+    if (!(a[0] % 2) || !(a[1] % 2)) cout << 0;
+    else cout << min(a[0], a[1]);
+}
